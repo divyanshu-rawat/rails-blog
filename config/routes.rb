b1c364c6076705b690265cfe4f22c4eb60716046
Divyanshu Rawat <divyanshu.r46956@gmail.com>
2019-01-20 17:29:48 +0100

    defined view for welcome controller, and re-directed requests to root to welcome controller index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get 'welcome/index' # Rails to map requests to http://localhost:3000/welcome/index to the welcome controller's index action.
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index'  #to map requests to the root of the application to the welcome controller's index action.
 end
