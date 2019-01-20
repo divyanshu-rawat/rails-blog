4e675ee5c12e1b8ba6a4b592ea6afdc0cfc3cf0c
Divyanshu Rawat <divyanshu.r46956@gmail.com>
2019-01-20 22:55:46 +0100

    created migration for article model creation in DB

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,10 @@
 class ArticlesController < ApplicationController
 	def new
 		# You're getting this error now because Rails expects plain actions like this one to have views associated with them to display their information. With no view available, Rails will raise an exception.
-		
+
+	end
+
+	def create
+		render plain: params[:article].inspect
 	end
 end
