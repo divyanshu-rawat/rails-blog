76611a9c74fe8b33734781a615fa6110b9f8ab1f
Divyanshu Rawat <divyanshu.r46956@gmail.com>
2019-01-20 23:26:28 +0100

    written #show action and #create action for the Articles controller.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,32 @@
 class ArticlesController < ApplicationController
+
+	def show
+		@article = Article.find(params[:id]) #We also use an instance variable (prefixed with @) to hold a reference to the article object. 
+		# We do this because Rails will pass all instance variables to the view.
+	end
+
 	def new
 		# You're getting this error now because Rails expects plain actions like this one to have views associated with them to display their information. With no view available, Rails will raise an exception.
-
 	end
 
 	def create
-		render plain: params[:article].inspect
+		@article = Article.new(article_params)
+		@article.save
+		redirect_to @article #@article.save is responsible for saving the model in the database
+		#we redirect the user to the show action, which we'll define later.
 	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	def edit
+	end
+
+	private
+		def article_params
+			params.require("article").permit(:title, :text)
+		end
 end
