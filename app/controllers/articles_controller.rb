de2451b76789cbd32aae9bc4a64155785bce888f
Divyanshu Rawat <divyanshu.r46956@gmail.com>
2019-01-21 00:58:29 +0100

    done with CR part of the https verbs in rails application 🚀

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,28 @@
 class ArticlesController < ApplicationController
 
+	def index
+		@articles = Article.all
+	end
+	
 	def show
 		@article = Article.find(params[:id]) #We also use an instance variable (prefixed with @) to hold a reference to the article object. 
 		# We do this because Rails will pass all instance variables to the view.
 	end
 
 	def new
+		@article = Article.new
 		# You're getting this error now because Rails expects plain actions like this one to have views associated with them to display their information. With no view available, Rails will raise an exception.
 	end
 
 	def create
 		@article = Article.new(article_params)
-		@article.save
-		redirect_to @article #@article.save is responsible for saving the model in the database
+
+		if @article.save
+			redirect_to @article
+		else
+			render 'new'
+		end
+		#@article.save is responsible for saving the model in the database
 		#we redirect the user to the show action, which we'll define later.
 	end
 
